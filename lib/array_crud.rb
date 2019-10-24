135a77cd4c52affca735c0db99d6d14ed4fa7916
Paul O <paul.orawiec@gmail.com>
2019-10-24 05:26:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_array = ["alpha", "beta", "gamma"]
-  my_array.push("arrays!")
+  my_array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
